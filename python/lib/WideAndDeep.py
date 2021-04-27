ab874f5770bf696e02d21fa30ac3814681c116da
ZHANAFENG <jamesbound859@gmail.com>
2021-04-27 08:54:14 +0800

    first commit

---
+++ python/lib/WideAndDeep.py
@@ -0,0 +1,6 @@
+import numpy as np
+import tensorflow as tf
+
+class WideAndDeep(tf.estimator.Estimator):
+    def __init__(self):
+        pass
---
+++ python/lib/utils/create_record.py
@@ -0,0 +1,8 @@
+import os
+import tensorflow as tf
+
+inpath = '../../data/dac_sample/dac_sample.txt'
+outpath = '../../data/train.tfrecords'
+
+def create_record():
+    pass
