7f5114ba60d0eebcdfd202b5353013ea362a208d
ZHANAFENG <jamesbound859@gmail.com>
2021-05-02 21:40:17 +0800

    add file

--- python/lib/WideAndDeep.py
+++ python/lib/WideAndDeep.py
@@ -1,6 +1,21 @@
 import numpy as np
-import tensorflow as tf
+import tensorflow._api.v2.compat.v1 as tf
+tf.disable_v2_behavior()
+
+outpath = '../../../data/train.tfrecords'
 
 class WideAndDeep(tf.estimator.Estimator):
-    def __init__(self):
+    def __init__(self, random_seed=2021):
+        self._init_graph_()
+        self.random_seed = random_seed
+
+    def _init_graph_(self):
+        self.graph = tf.Graph()
+        with self.graph.as_default():
+            tf.set_random_seed(self.random_seed)
+
         pass
+
+
+if __name__=="__main__":
+    dataset = tf.data.TFRecordDataset(outpath)
--- python/lib/utils/create_record.py
+++ python/lib/utils/create_record.py
@@ -1,8 +1,31 @@
 import os
+import timeit
+from itertools import islice
 import tensorflow as tf
 
-inpath = '../../data/dac_sample/dac_sample.txt'
-outpath = '../../data/train.tfrecords'
+inpath = '../../../data/data.csv'
+outpath = '../../../data/train.tfrecords'
 
 def create_record():
-    pass
+    start_time = timeit.default_timer()
+    writer = tf.io.TFRecordWriter(outpath)
+
+    f = open(inpath, "r")
+    for line in islice(f, 1, None):
+        data = line.split(',')
+        label = int(data[0])
+        features = [float(i) if i!="" else float(0) for i in data[1:]]
+
+        example = tf.train.Example(features=tf.train.Features(feature={
+            "label" : tf.train.Feature(int64_list = tf.train.Int64List(value=[label])),
+            "features" : tf.train.Feature(float_list = tf.train.FloatList(value=features)),
+        }))
+    writer.write(example.SerializeToString())
+    writer.close()
+
+    end_time = timeit.default_timer()
+    print("\nThe pretraining process ran for {0} minutes\n".format((end_time - start_time) / 60))
+
+
+if __name__ == "__main__":
+    create_record()
